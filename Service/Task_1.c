09c4a417ae75cfbb31bd08ef8bdec8099e9306b2
DESKTOP-3L5G4RI\Ahmed <a.salahhelmy2001@gmail.com>
2024-01-20 23:13:17 +0200

    .

--- Service/Task_1.c
+++ Service/Task_1.c
@@ -12,73 +12,20 @@
 #include "BIT_Math.h"
 #include "STD_TYPES.h"
 
-#define F_CPU 8000000UL
-#include <avr/delay.h>
-
-#include "DIO_interface.h"
-#include "SW_interface.h"
-
-
-void SW_voidPinInit(SW_Type *SW_structConfig)
+void SwitchHandle(void *pv)
 {
-	DIO_enumSetPinDirection(SW_structConfig->Port,SW_structConfig->Pin,SW_PIN_INPUT);
+	u8 Local_u8_semState1; /*Variable to store return state of SemTake*/
 
-
-	/*if State is PUll UP Internal in MC*/
-	if (SW_structConfig->Pull_State == SW_INTERNAL_PULL_UP)
+	while(1)
 	{
-		DIO_enumSetPinValue(SW_structConfig->Port,SW_structConfig->Pin,SW_PIN_HIGH);
-	}
-}
-
-
-u8 SW_u8GetPressed(SW_Type *SW_structconfig)
-{
-	/*the return  */
-	static	u8 Local_u8PinVal = SW_NOT_PRESSED ;
-	static	u8 Local_u8Result = SW_NOT_PRESSED ;
-
-	/*if the State is Pull_up General*/
-	if ((SW_structconfig ->Pull_State == SW_INTERNAL_PULL_UP)  || (SW_structconfig ->Pull_State == SW_EXTERNAL_PULL_UP))
-	{
-		/*Get value from Switch*/
-		DIO_enumGetPinValue(SW_structconfig->Port,SW_structconfig->Pin,&Local_u8PinVal);
-
-		/*Pressed*/
-		if( Local_u8PinVal == 0 )
+		/*Taking semaphoreA*/
+		Local_u8_semState1=xSemaphoreTake(SemA,0);
+		if(Local_u8_semState1==pdPASS)
 		{
-			Local_u8Result= SW_PRESSED;
-			_delay_ms(40);
-		}
-		/*Not Pressed*/
-		else if( Local_u8PinVal == 1 )
-		{
-			Local_u8Result= SW_NOT_PRESSED;
-			_delay_ms(40);
-
+			/*Reading SW State & access Led_SW_state shared resource */
+			Led_SW_state=SW_u8GetPressed(&SWConfig);
+			vTaskDelay(50);
 		}
 	}
 
-	else if (SW_structconfig->Pull_State == SW_EXTERNAL_PULL_DOWN)
-	{
-		/*Get value from Switch*/
-		DIO_enumGetPinValue(SW_structconfig->Port,SW_structconfig->Pin,&Local_u8PinVal);
-
-		/*Pressed*/
-		if( Local_u8PinVal == 1 )
-		{
-			Local_u8Result= SW_PRESSED;
-			_delay_ms(40);
-
-		}
-		/*Not Pressed*/
-		else if( Local_u8PinVal == 0 )
-		{
-			Local_u8Result= SW_NOT_PRESSED;
-			_delay_ms(40);
-
-		}
-	}
-
-	return Local_u8Result;
 }
