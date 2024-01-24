899c10d3c43d770e542615d4e82666aedf141191
Ahmed Salah <a.salahhelmy2001@gmail.com>
2024-01-24 22:12:12 +0200

    LEDControl Task

---
+++ Service/LEDControl/LEDControl.c
@@ -0,0 +1,30 @@
+#include"LEDControl.h"
+
+
+/*Free RTOS includes*/
+#include"FreeRTOS.h"
+#include"task.h"
+#include "semphr.h"
+
+/********************************************************/
+void LEDControl(void *pv)
+{
+	u8 Local_u8_semState1; /*Variable to store return state of SemTake*/
+
+	while(1)
+	{
+		/*Taking semaphoreA*/
+		Local_u8_semState1=xSemaphoreTake(SemA,0);
+		if(Local_u8_semState1==pdPASS)
+		{
+			/*checking on Led_SW_state & access shared resource */
+			if(Led_SW_state==SW_PRESSED)
+			{
+				LED_voidPinToggle(&LedConfig);
+			}
+
+		}
+
+	}
+}
+/********************************************************/
---
+++ Service/LEDControl/LEDControl.h
@@ -0,0 +1,6 @@
+#ifndef LED_H_
+#define LED_H_
+
+void LEDControl(void *pv)
+
+#endif 
