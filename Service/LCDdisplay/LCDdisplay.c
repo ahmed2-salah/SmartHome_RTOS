2b9fb0fac1a8ea959430e71dc9efab46986f9e7c
Ahmed Salah <a.salahhelmy2001@gmail.com>
2024-01-24 22:10:41 +0200

    LCD Task

---
+++ Service/LCDdisplay/LCDdisplay.c
@@ -0,0 +1,42 @@
+#include"LCDdisplay.h"
+
+
+/*Free RTOS includes*/
+#include"FreeRTOS.h"
+#include"task.h"
+#include "semphr.h"
+
+
+
+/********************************************************/
+void LCDdisplay(void *pv)
+{
+	while(1)
+	{
+		/*Led State*/
+		CLCD_voidGoToXY(0,0);
+		if(Led_SW_state==LED_HIGH)
+		{
+			CLCD_voidSendString("Led is ON");
+		}
+		else
+		{
+			CLCD_voidSendString("Led is OFF");
+		}
+
+		/*Door State*/
+		CLCD_voidGoToXY(1,0);
+		if(door_SW_state==SW_PRESSED)
+		{
+			CLCD_voidSendString("Door is open");
+		}
+		else
+		{
+			CLCD_voidSendString("Door is close");
+		}
+		/*Temperature Value*/
+		CLCD_voidGoToXY(2,0);
+		CLCD_voidSendData(temp_value);
+	}
+}
+/********************************************************/
---
+++ Service/LCDdisplay/LCDdisplay.h
@@ -0,0 +1,6 @@
+#ifndef LCD_H_
+#define LCD_H_
+
+void LCDdisplay(void *pv);
+
+#endif 
