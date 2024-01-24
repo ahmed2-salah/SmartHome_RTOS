afd1addb97b61faee10e4f54e9ac2ed251f30eb6
Ahmed Salah <a.salahhelmy2001@gmail.com>
2024-01-24 22:05:56 +0200

    BuzzerContrlo Task

---
+++ Service/BuzzerControl/BuzzerControl.c
@@ -0,0 +1,29 @@
+#include "BuzzerContrlo.h"
+/*Free RTOS includes*/
+#include"FreeRTOS.h"
+#include"task.h"
+#include "semphr.h"
+
+
+void BuzzerControl(void *pv)
+{
+	u8 Local_u8_semState1; /*Variable to store return state of SemTake*/
+
+	while(1)
+	{
+		/*Taking semaphoreB*/
+		Local_u8_semState1=xSemaphoreTake(SemB,0);
+		if(Local_u8_semState1==pdPASS)
+		{
+			/*Check on door_SW & access door_SW_state shared resource*/
+			if(door_SW_state==SW_PRESSED)
+			{
+				BUZZ_voidTurnOn(&buzzerConfig);
+			}
+			else
+			{
+				BUZZ_voidTurnOff(&buzzerConfig);
+			}
+		}
+	}
+}
---
+++ Service/BuzzerControl/BuzzerControl.h
@@ -0,0 +1,9 @@
+#ifndef BUZZER_H_
+#define BUZZER_H_
+
+
+
+void BuzzerControl(void *pv);
+
+
+#endif 
